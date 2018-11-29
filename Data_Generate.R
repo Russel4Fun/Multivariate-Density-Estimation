c67a5bb5ee23b1c5415afbc8febebfedc6b4a63d
Russel4Fun <45435499+Russel4Fun@users.noreply.github.com>
2018-11-29 14:16:54 -0500

    upload

--- Data_Generate.R
+++ Data_Generate.R
@@ -1,5 +1,6 @@
 set.seed(740)
 library(datasets)
+library(mice)
 # A hierarchial model to generate 2-dimensional data
 mu.true=function(x){exp(x/6)-x+log(x^4+1)}
 sigma.true=function(x){(x^2)*exp(-abs(x))}
@@ -41,7 +42,26 @@ missing_z_index_1 = sample.int(100, size = 100 * miss_z_prob1, replace = F)
 missing_data2 <- data2
 missing_data2[missing_x_index_1,1] = NA
 missing_data2[missing_y_index_1,2] = NA
-missing_data2[missing_x_index_1,3] = NA
+missing_data2[missing_z_index_1,3] = NA
 
 # real iris data
 data3 = iris[,1:4]
+
+# real windspeed data
+data4 = mice::windspeed
+n4 = dim(data4)[1]
+missing_a_prob = 0.2;missing_b_prob = 0.2;missing_c_prob = 0.2
+missing_d_prob = 0.2;missing_e_prob = 0.2;missing_f_prob = 0.2
+missing_a_index = sample.int(n4, size = n4 * missing_a_prob, replace = F)
+missing_b_index = sample.int(n4, size = n4 * missing_b_prob, replace = F)
+missing_c_index = sample.int(n4, size = n4 * missing_c_prob, replace = F)
+missing_d_index = sample.int(n4, size = n4 * missing_d_prob, replace = F)
+missing_e_index = sample.int(n4, size = n4 * missing_e_prob, replace = F)
+missing_f_index = sample.int(n4, size = n4 * missing_f_prob, replace = F)
+missing_data4 <- data4
+missing_data4[missing_a_index,1] = NA
+missing_data4[missing_b_index,2] = NA
+missing_data4[missing_c_index,3] = NA
+missing_data4[missing_d_index,4] = NA
+missing_data4[missing_e_index,5] = NA
+missing_data4[missing_f_index,6] = NA
