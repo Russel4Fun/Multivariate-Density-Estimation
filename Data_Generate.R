b465dcb638e403418e0d8cec712088da505d7135
Russel4Fun <45435499+Russel4Fun@users.noreply.github.com>
2018-11-29 00:31:18 -0500

    2 simulation and 1 real data

--- Data_Generate.R
+++ Data_Generate.R
@@ -1,4 +1,5 @@
 set.seed(740)
+library(datasets)
 # A hierarchial model to generate 2-dimensional data
 mu.true=function(x){exp(x/6)-x+log(x^4+1)}
 sigma.true=function(x){(x^2)*exp(-abs(x))}
@@ -30,6 +31,17 @@ missing_data1 <- cbind(x1,y1)
 
 # another simulation data
 require(MASS)
+miss_x_prob1 = 0.3; miss_y_prob1 = 0.3;miss_z_prob1 = 0.3
 sigma <- matrix(data = c(1, 1.2, 2.2, 1.2, 1, 1.2, 2.2, 1.2, 1), nrow = 3)
 sigma <- sigma%*%t(sigma)
 data2 <- mvrnorm(n = 100, mu = c(5, 5, 5), Sigma = sigma)
+missing_x_index_1 = sample.int(100, size = 100 * miss_x_prob1, replace = F)
+missing_y_index_1 = sample.int(100, size = 100 * miss_y_prob1, replace = F)
+missing_z_index_1 = sample.int(100, size = 100 * miss_z_prob1, replace = F)
+missing_data2 <- data2
+missing_data2[missing_x_index_1,1] = NA
+missing_data2[missing_y_index_1,2] = NA
+missing_data2[missing_x_index_1,3] = NA
+
+# real iris data
+data3 = iris[,1:4]
