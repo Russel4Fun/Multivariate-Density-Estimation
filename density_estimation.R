2025a24fbcc9fbde3042ba70aa6692d195eee6dd
Miao Yu <myu12@ncsu.edu>
2018-11-29 23:36:37 -0500

    Miao

--- density_estimation.R
+++ density_estimation.R
@@ -25,7 +25,7 @@ density_estimation <- function(data,m,N.iter,n.iter,N.burn) {
   
   # subset to get a dataset whose all dimensions are observed
   part_data=data[complete.cases(data), ]
-  part_n=nrow(com_data)
+  part_n=nrow(part_data)
   
   # compute the knots 
   knots = matrix(0,p,m)
@@ -78,3 +78,78 @@ density_estimation <- function(data,m,N.iter,n.iter,N.burn) {
     
   }
 }
+
+
+# density_estimation_old
+
+density_estimation_old <- function(data,m,N.iter,n.iter,N.burn) {
+  
+  n=nrow(data)
+  p=ncol(data)
+  
+  # initialize parameters
+  K <- matrix(0,N.iter,n)
+  theta <- matrix(0,N.iter,m)
+  alpha <- matrix(0,N.iter,p)
+  
+  theta[1,]<-rep(1/m,m)
+  alpha[1,]<-rep(1.5,p)
+  K[1,] <- sample.int(m,size = n, replace = TRUE)
+  
+  
+  # subset to get a dataset whose all dimensions are observed
+  part_data=data[complete.cases(data), ]
+  part_n=nrow(part_data)
+  
+  # compute the knots 
+  knots = matrix(0,p,m)
+  x=part_data[,1]
+  index_order=round((1:(m-2))*part_n/(m-1))
+  index_order=ifelse(index_order==0,1,index_order)
+  knots[1,] = c(min(x),sort(x)[index_order],max(x));
+  for (i in 2:p) {
+    y <- part_data[,i]
+    y.ord=y[order(x)]
+    index_order=round((1:(m-2))*part_n/(m-1))
+    index_order=ifelse(index_order==0,1,index_order)
+    knots[i,]=y.ord[c(1,index_order,part_n)]
+  }
+  
+  
+  
+  # If no missing values, only use Gibbs_sampler to sample parameters
+  if(part_n==n){
+    
+    list=fix_beta_2(data, m, alpha[1,], K[1,], theta[1,], knots, N.iter, N.burn)
+    
+    return(list(data=data,theta=list$theta,alpha=list$alpha,beta=list$beta,K=list$K,knots=knots))
+  }
+  
+  
+  # If the data has missing values, use missing_value_sampler and Gibbs_sampler
+  if(part_n!=n){
+    
+    # record the location where data is missing
+    miss_ind = matrix(0, n, p)
+    for (i in 1:n){
+      miss_ind[i,] <- is.na(data[i,])
+    }
+    
+    for(l in 1:(N.iter-1)){
+      
+      newdata=missing_value_sampler_lastyear(data, miss_ind, knots, theta[l,], alpha[l,])
+      
+      para=fix_beta_2(newdata, m, alpha[l,], K[l,], theta[l,], knots, n.iter, n.iter-1)
+      
+      alpha[l+1,]=para$alpha
+      theta[l+1,]=para$theta
+      K[l+1,]=para$K
+      
+    }  
+    
+    return(list(newdata=newdata,theta=theta[(N.burn+1):N.iter,],alpha=alpha[(N.burn+1):N.iter,],K=K[(N.burn+1):N.iter,],knots=knots))
+    
+  }
+}
+
+
--- missing_value_sampler.R
+++ missing_value_sampler.R
@@ -71,8 +71,55 @@ missing_value_sampler <- function(data, miss_ind, knots, theta, alpha, beta) {
             h=function(x){cdf(x)-u} 
             uniroot(h,interval=c(knots[j,k]-10,knots[j,k]+10))$root}
           u=runif(1)
-          while(u>0.99 | u<0.001){u=runif(1)}
-          complete_data[i,j]=sapply(u,qf)
+          while(u>0.99 | u<0.01){u=runif(1)}
+          t=try(sapply(u,qf))
+          if(isTRUE(class(t)=="try-error")) { complete_data[i,j]=mean(data, na.rm=TRUE) } 
+          else {complete_data[i,j]=t} 
+      }
+    }  
+  }
+  
+  return(complete_data)
+  
+}
+
+
+# Last year method
+missing_value_sampler_lastyear <- function(data, miss_ind, knots, theta, alpha) {
+  
+  n=nrow(data)
+  p=ncol(data)
+  m=length(theta)
+  
+  complete_data=matrix(0,n,p)
+  
+  for (i in 1:n){
+    
+    miss_ind_i=c(1:p)[miss_ind[i,]==TRUE]
+    nonmiss_ind_i=c(1:p)[miss_ind[i,]==FALSE]
+    
+    if(length(nonmiss_ind_i)==p){complete_data[i,]=data[i,]}
+    
+    else{
+      complete_data[i,]=data[i,]
+      
+      freq=NULL
+      for(k in 1:m){
+        
+        nomis=1
+        for (j in nonmiss_ind_i){
+          nomis=nomis*dnorm(data[i,j],knots[j,k],alpha[j])
+        }
+        freq=c(freq,theta[k]*nomis)
+        
+      }
+      
+      prob=freq/sum(freq)
+      k=sample(1:m,1,prob=prob)
+      
+      for(j in miss_ind_i){
+        
+       complete_data[i,j]=rnorm(1,knots[j,k],alpha[j]) 
       }
     }  
   }
